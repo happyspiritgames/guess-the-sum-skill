f224f5fe66742c56b5c11d782c53d0c7bb55641c
Dave Mount <dave@happyspiritgames.com>
2018-08-04 18:26:30 -0700

    Pretty much working on the happy paths. Work when someone answers with a non-number, too.

--- lambda/custom/index.js
+++ lambda/custom/index.js
@@ -18,23 +18,69 @@ const LaunchRequestHandler = {
   },
 };
 
+const pickRandomFromRange = (from = 0, to = 10) => {
+  // remember to offset by from to start at the correct point in the range where from != 0
+  return Math.floor(Math.random() * (to - from + 1)) + from;
+};
+
 const announceWinner = (handlerInput) => {
-  const speechText = 'And we have a winner. I think. Now, where did I put my calculator?'
+  let speechText;
+  const { slots } = handlerInput.requestEnvelope.request.intent;
+
+  const playerSecretNumber = parseInt(slots.playerSecretNumber.value);
+  const playerGuess = parseInt(slots.playerGuess.value);
+  if (Number.isNaN(playerSecretNumber) || Number.isNaN(playerGuess)) {
+    speechText = 'One of the answers you gave me is not a number. You forfeit.';
+    return handlerInput.responseBuilder
+      .speak(speechText)
+      .withSimpleCard('Guess the Sum', speechText)
+      .getResponse();
+  }
+
+  // TODO re-ask when given numbers are out of bounds
+
+
+  // skill takes turn
+  const skillSecretNumber = pickRandomFromRange();
+  let skillGuess;
+  do {
+    skillGuess = pickRandomFromRange(skillSecretNumber, 20);
+  } while (skillGuess === playerGuess);
+
+  const sum = playerSecretNumber + skillSecretNumber;
+  const playerDelta = Math.abs(playerGuess - sum);
+  const skillDelta = Math.abs(skillGuess - sum);
+
+  speechText = `My guess is ${skillGuess}. `;
+  speechText += 'Now I will calculate the sum. '
+  speechText += `My secret number is ${skillSecretNumber}, so the sum is ${sum}. `;
+
+  if (playerDelta < skillDelta) {
+    speechText += 'Your guess is closer to the sum, so you win. That is worth two points. ';
+    if (playerDelta === 0) {
+      speechText += 'Also, you guessed the sum exactly, so you get an extra point. Well done. ';
+    }
+  } else if (playerDelta > skillDelta) {
+    speechText += 'My guess is closer to the sum, so I win. ';
+    if (skillDelta === 0) {
+      speechText += 'Also, I guessed the sum exactly. Aren\'t you impressed? ';
+    }
+  } else {
+    speechText += 'We tied. Our guesses are the same distance from the sum. We each get a point. ';
+  }
+
   return handlerInput.responseBuilder
     .speak(speechText)
     .withSimpleCard('Guess the Sum', speechText)
     .getResponse();
-}
+};
 
 const verifySlots = (handlerInput) => {
   const { intent } = handlerInput.requestEnvelope.request;
-
-  // TODO use response interceptor to validate slot values
-
   return handlerInput.responseBuilder
     .addDelegateDirective(intent)
     .getResponse();
-}
+};
 
 const PlayGameIntentHandler = {
   canHandle(handlerInput) {
@@ -42,12 +88,12 @@ const PlayGameIntentHandler = {
       && handlerInput.requestEnvelope.request.intent.name === 'PlayGameIntent';
   },
   handle(handlerInput) {
-    const { dialogState, intent } = handlerInput.requestEnvelope.request;
+    const { dialogState } = handlerInput.requestEnvelope.request;
 
     if (dialogState === 'COMPLETED') {
-      return announceWinner(handlerInput)
+      return announceWinner(handlerInput);
     } else {
-      return verifySlots(handlerInput)
+      return verifySlots(handlerInput);
     }
   },
 };
---
+++ sample-json/request.js
@@ -0,0 +1,23 @@
+const sample = {
+  type: 'IntentRequest',
+  requestId: 'amzn1.echo-api.request.7570902c-eb2d-424a-9d3c-e02da196dace',
+  timestamp: '2018-08-04T20:00:57Z',
+  locale: 'en-US',
+  intent: {
+    name: 'PlayGameIntent',
+    confirmationStatus: 'NONE',
+    slots: {
+      playerSecretNumber: {
+        name: 'playerSecretNumber',
+        value: '6',
+        confirmationStatus: 'NONE'
+      },
+      playerGuess: {
+        name: 'playerGuess',
+        value: '7',
+        confirmationStatus: 'NONE'
+      }
+    }
+  },
+  dialogState: 'COMPLETED'
+}
